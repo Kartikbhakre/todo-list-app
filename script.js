71d6f931882c4a9f46fe80042fcc2419ff34d4c1
kartik Bhakre <kartikbhakre95@gmail.com>
2026-04-20 12:19:04 +0530

    Refactor todo app to include checkbox and edit functionality

--- script.js
+++ script.js
@@ -1,45 +1,66 @@
-let input = document.querySelector("#input");
+let input = document.getElementById("inp") ;
 let ul = document.querySelector("ul");
 
-let arr = JSON.parse(localStorage.getItem("todos"))||[];
+arr = JSON.parse(localStorage.getItem("todos"))||[] ;
 
 function save(){
- localStorage.setItem("todos",JSON.stringify(arr));
+    localStorage.setItem("todos" , JSON.stringify(arr));
 }
-
-function add(){
-    if(input.value.trim()=="")return ;
-    arr.push(input.value);
-    save();
-    display();
-    input.value="";
-    
-    function display(){
-        ul.innerHTML = "";
-        arr.forEach((val, idx)=>{
-        let li  = document.createElement("li");
-        li.textContent = val;
-
-        let del = document.createElement("button");
-        del.textContent = "delete" ;
-        del.onclick = ()=>{
-            arr.splice(idx,1);
-            save();
-            display();
-        }
-
-        let edit = document.createElement("button");
-        edit.textContent = "Edit";
-        edit.onclick = ()=>{
-            let New = prompt("Enter new value " , val);
-            if(New.trim()=="")return ;
-            arr[idx]=New ;
-            save();
-            display();
-        }
-        li.append(del);
-        li.append(edit);
-        ul.append(li);
-        });
+function addtodo(){
+    if(input.value.trim()==""){
+        alert("please enter value:");
+        return ;
+    }
+    else{
+        arr.push({data:input.value , done:false});
+        save();
+        display();
+        input.value = "";
     }
 }
+ function display(){
+    ul.innerHTML = "";
+
+    arr.forEach((val , idx)=>{
+     let checkbox = document.createElement("input");
+     checkbox.type="checkbox";  
+     checkbox.onchange =  ()=>{
+        if(checkbox.checked == true){
+        span.style.textDecoration = 'line-through';
+        }
+        else{
+        span.style.textDecoration = 'none';
+        }
+        arr[idx].done = checkbox.checked ;
+        save();
+
+     } 
+     let li = document.createElement("li");
+     let span = document.createElement("span");
+     span.textContent = arr[idx].data ;
+
+     let del = document.createElement("button");
+     del.textContent = "delete" ;
+     del.onclick = ()=>{
+        arr.splice(idx , 1);
+       save();
+        display()
+     }
+     let edit = document.createElement("button");
+     edit.textContent = "edit";
+     edit.onclick = ()=>{
+        let New = prompt("enter new value ",val);
+        if(New.trim()=="")return ;
+        arr[idx].data = New ;
+        save();
+        display();
+     } 
+     ul.appendChild(li);
+     li.appendChild(checkbox);
+    li.appendChild(span);
+     li.appendChild(del);
+     li.appendChild(edit);
+    });
+ }
+ 
+ window.onload = display;
